9bd9288f9195e3255c3f361c725b865b054e422e
Batsal Awaley <batsal@vayu.com.au>
2018-10-12 21:50:20 +0545

    implement rubocop

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length: {maximum: 140, minimum:20},
+  validates :content, length: { maximum: 140, minimum: 20 },
                       presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219100442) do
-
+ActiveRecord::Schema.define(version: 20_161_219_100_442) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "microposts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'microposts', force: :cascade do |t|
+    t.text     'content'
+    t.integer  'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string   'name'
+    t.string   'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
